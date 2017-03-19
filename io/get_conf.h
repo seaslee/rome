38470ac59a192df69eeb397fc0982461d6165963
xuxinchao <xxinchao@gmail.com>
2017-03-19 20:20:45 +0800

    add unittest for read proto text file

--- io/get_conf.h
+++ io/get_conf.h
@@ -4,12 +4,29 @@
 #include "google/protobuf/text_format.h"
 #include "google/protobuf/io/zero_copy_stream_impl.h"
 #include "../proto/snoopy.pb.h"
+#include <fcntl.h>
+#include <cstdio>
+#include "../common/com_def.h"
+#include "../common/logging.h"
+
 
 namespace snoopy {
 namespace io {
 
-int read_net_proto_from_text_file(const string & f_name, 
-                                NetParamter & net_param);
+inline int read_net_proto_from_text_file(const string & f_name, 
+                                NetParameter & net_param) {
+    int file_handle = open(f_name.c_str(), O_RDONLY);
+    if (!file_handle) {
+        LOG_FATAL << "net configure file: " << f_name <<" not exists!";
+        return snoopy::FAILURE;
+    }
+    google::protobuf::io::FileInputStream file_input(file_handle);
+    if (!google::protobuf::TextFormat::Parse(&file_input, &net_param)) {
+        LOG_FATAL << "net configure file: " << f_name <<" read failed!";
+        return snoopy::FAILURE;
+    }
+    return snoopy::SUCCESS;
+}
                                   
 }
 }
